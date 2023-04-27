404ac5e871b5097d01f29506c1d75963fbd0fcf9
quinlin Hopperstad <quinlin.hopperstad@ucdenver.edu>
2023-04-27 13:17:34 -0600

    added people

--- tinder-project/src/TinderCards.js
+++ tinder-project/src/TinderCards.js
@@ -1,11 +1,25 @@
-import React from "react";
-import './TinderCards.css';
+import React, {useState} from "react";
+import "./TinderCards.css";
 
 function TinderCards(){
-    return
+    const [people, setPeople] = useState([
+        {
+            name: 'Ben Affleck',
+            url: "https://www.hollywoodreporter.com/wp-content/uploads/2019/11/ben_affleck_credit_john_russo.jpg?w=1296&h=730&crop=1",
+        },
+        {
+            name: 'Elon Musk',
+            url: "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+        },
+    ]);
+
+    return (
         <div className = "tinderCards">
-            
-        </div>;
+            {people.map((person) => (
+                <h1>{person.name}</h1>
+            ))}
+        </div>
+    );
 }
 
 export default TinderCards;
